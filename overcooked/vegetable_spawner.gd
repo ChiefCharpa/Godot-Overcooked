81012799949f98a9654e3534a63f675d1e8babe8
llama2474 <wiktorledzion@gmail.com>
2025-02-10 12:57:51 +0000

    Spawn Rework
    You can now only spawn a "tomato" if you are close to the box and press "E", there is also a 1 second cooldown before you can spawn it again

--- overcooked/vegetable_spawner.gd
+++ overcooked/vegetable_spawner.gd
@@ -1,10 +1,26 @@
 extends Node3D
 
-@onready var spawntimer = $Timer
-
+@onready var Player = $"../Player"
+@onready var Veg = $"."
+var DetectionRange = 1.4
+var InRange = false
+var SpawnDelay = true
 const VEGETABLE = preload("res://Vegetable.tscn")
+	
 
-func _on_timer_timeout() -> void:
-	var newVegetable = VEGETABLE.instantiate()
-	get_parent().add_child(newVegetable)
-	newVegetable.global_position=global_position+Vector3(0,0.4,0)
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed() and SpawnDelay and InRange:
+		if event.keycode == KEY_E:
+			var newVegetable = VEGETABLE.instantiate()
+			get_parent().add_child(newVegetable)
+			newVegetable.global_position=global_position+Vector3(0,0.4,0)
+			SpawnDelay = false
+			await get_tree().create_timer(1).timeout #delays spawning by 1 second
+			SpawnDelay = true
+
+func _process(delta): #So this could be change so it does an area but this is simple and works
+	var distance = Veg.transform.origin.distance_to(Player.transform.origin)
+	if distance < DetectionRange:
+		InRange = true
+	else:
+		InRange =false
