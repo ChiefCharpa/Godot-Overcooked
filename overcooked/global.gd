a3902923e38b5b9bb4f52086c5998febd50ac157
Ted Jenkinson <158061840+TCursed@users.noreply.github.com>
2025-04-07 16:08:26 +0100

    Chopped Foods
    Added chopped foods.
    Organised a bit more, Sorry for confilcts.

--- overcooked/global.gd
+++ overcooked/global.gd
@@ -1,13 +1,19 @@
 extends Node3D
 
 var VegDictionary = {
-	"Tomato" : preload("res://Objects/Food preloads/Tomato.tscn"),
-	"Fish" : preload("res://Objects/Food preloads/Fish.tscn"),
-	"Onion" : preload("res://Objects/Food preloads/Onion.tscn"),
-	"Mushroom" : preload("res://Objects/Food preloads/Mushroom.tscn"),
-	"Lettuce" : preload("res://Objects/Food preloads/Lettuce.tscn"),
-	"Meat" : preload("res://Objects/Food preloads/Meat.tscn"),
-	"Plate" : preload("res://Plate.tscn")
+	"Tomato" : preload("res://Objects/Food preloads/Raw/Tomato.tscn"),
+	"Fish" : preload("res://Objects/Food preloads/Raw/Fish.tscn"),
+	"Onion" : preload("res://Objects/Food preloads/Raw/Onion.tscn"),
+	"Mushroom" : preload("res://Objects/Food preloads/Raw/Mushroom.tscn"),
+	"Lettuce" : preload("res://Objects/Food preloads/Raw/Lettuce.tscn"),
+	"Meat" : preload("res://Objects/Food preloads/Raw/Meat.tscn"),
+	"Plate" : preload("res://Objects/Containers/Plate.tscn"),
+	"Chopped_Tomato" : preload("res://Objects/Food preloads/Chopped/Chopped_Tomato.tscn"),
+	"Chopped_Fish" : preload("res://Objects/Food preloads/Chopped/Chopped_Fish.tscn"),
+	"Chopped_Onion" : preload("res://Objects/Food preloads/Chopped/Chopped_Onion.tscn"),
+	"Chopped_Mushroom" : preload("res://Objects/Food preloads/Chopped/Chopped_Mushroom.tscn"),
+	"Chopped_Lettuce" : preload("res://Objects/Food preloads/Chopped/Chopped_Lettuce.tscn"),
+	"Chopped_Meat" : preload("res://Objects/Food preloads/Chopped/Chopped_Meat.tscn"),
 	}
 var Veglist = VegDictionary.keys()
 # Called when the node enters the scene tree for the first time.
