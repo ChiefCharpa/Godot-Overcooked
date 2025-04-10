68987c2432d48eef38d20f57a5c7eee226068185
llama2474 <wiktorledzion@gmail.com>
2025-04-10 16:54:52 +0100

    Added Chopping
    Press X to start chopping, freezes the player for 10 seconds. Need to do some bug fixes

--- overcooked/InteractionChecker.gd
+++ overcooked/InteractionChecker.gd
@@ -7,6 +7,7 @@ var action_processed = false #tracks if the action is processed
 var force = 0
 var pickupable = true
 
+
 func _ready() -> void:
 	player_inventory = get_parent().get_node("Inventory") #gets the player's inventory
 	inventory_node = get_node("/root/LevelNode/Player/Inventory") #gets the inventorys node path
@@ -20,18 +21,18 @@ func _process(delta):
 	var overlapping_bodies = get_overlapping_bodies()
 	body_to_activate = null #resets body_to_activate before checking for overlapping bodies
 	
-	overlapping_bodies.sort_custom(func(a, b): 
-		return a.global_transform.origin.distance_to(self.global_transform.origin) < b.global_transform.origin.distance_to(self.global_transform.origin)
-)
-	
 	for body in overlapping_bodies:
 		if body.has_method("_activate"):
 			resource_type = body.get_some_variable() #gets the resource type from the body
 			body_to_activate = body #sets body_to_activate to the current body
 	print(body_to_activate)
 
-	if Input.is_action_just_pressed("Chop") and body_to_activate != null and body_to_activate.has_method("Iscuttingboard"):
-		body_to_activate.call("_chop",self.get_parent())
+	if Input.is_action_just_pressed("Chop"): #if you press chop, then look again but this time for a counter
+		for body in overlapping_bodies:
+			body_to_activate = body
+			if body_to_activate.has_method("Iscuttingboard"):
+				resource_type = body.get_some_variable()
+				body_to_activate.call("_chop",self.get_parent())
 		
 	#checks if 'e' is pressed, if there is not an interatable obj, if the item in front is type food
 	#and that theres an item in the invt
