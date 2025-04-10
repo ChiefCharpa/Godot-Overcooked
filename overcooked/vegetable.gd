1c819b7a644a99cd0ef079fda1696ab51435711a
llama2474 <wiktorledzion@gmail.com>
2025-04-10 18:43:48 +0100

    Fixed the merge
    Fixed the merge and made it so the player picks up a vegetable when it is spawned

--- overcooked/vegetable.gd
+++ overcooked/vegetable.gd
@@ -5,8 +5,8 @@ var resource_type = "Food"
 
 func _activate(player_inventory):
 	if player_inventory and player_inventory.can_pickup ==true: #makes sure the player isnt holding something
-		player_inventory.add_resources(self.name) #adds tomato to inventory 
-		queue_free() #removes tomato object
+		player_inventory.add_resources(self) #adds tomato to inventory 
+		#queue_free() #removes tomato object
 
 
 func get_some_variable():
