3974cee0dad4ebe170170db41d6ab849cdd5d0ea
FeasibleSnail85 <98097096+FeasibleSnail85@users.noreply.github.com>
2025-05-08 13:51:44 +0100

    Main Menu Button Press

--- overcooked/controlButton.gd
+++ overcooked/controlButton.gd
@@ -5,26 +5,32 @@ var GameScene3 = preload("res://Level3.tscn")
 var GameScene4 = preload("res://level_4.tscn")
 func buttonPressed():
 	$"../ControlDisplay".displayControls()
+	$"ButtonSFX".play()
 
 func _ready():
 	self.pressed.connect(buttonPressed)
 
 
 func _on_level_1_pressed() -> void:
+	$"ButtonSFX".play()
 	get_tree().change_scene_to_packed(GameScene1)
 
 
 func _on_level_2_pressed() -> void:
+	$"ButtonSFX".play()
 	get_tree().change_scene_to_packed(GameScene2)
 
 
 func _on_level_3_pressed() -> void:
+	$"ButtonSFX".play()
 	get_tree().change_scene_to_packed(GameScene3)
 
 func _on_level_4_pressed() -> void:
+	$"ButtonSFX".play()
 	get_tree().change_scene_to_packed(GameScene4)
 
 
 func _on_coop_pressed() -> void:
+	$"ButtonSFX".play()
 	Global.coop = not Global.coop
 	print(Global.coop)
